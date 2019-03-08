f4ab1a1dc178d6c7ac9ddb0a11eeda08c83b4dcb
Neail <blacated@gmail.com>
2019-03-08 16:19:52 +0800

    add interface

--- lib/redis/redis.interface.ts
+++ lib/redis/redis.interface.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import { RedisOptions } from 'ioredis';
+import { Redis } from 'ioredis';
 
 // tslint:disable-next-line:no-empty-interface
-export interface RedisClient extends RedisOptions {}
+export interface RedisClient extends Redis {}
